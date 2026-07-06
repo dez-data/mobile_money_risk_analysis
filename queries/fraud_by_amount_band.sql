f19c9b19d07d78a56d007fadaf073fecc21ba085
dez-data <samuelasteragondeze@gmail.com>
2026-07-06 03:48:02 +0300

    Update fraud_by_amount_band.sql

--- queries/fraud_by_amount_band.sql
+++ queries/fraud_by_amount_band.sql
@@ -3,11 +3,11 @@ WITH amount_bands AS (
         transaction_id,
         is_fraud,
         CASE
-            WHEN amount < 500            THEN '1. Under 500'
-            WHEN amount BETWEEN 500  AND 999   THEN '2. 500-999'
-            WHEN amount BETWEEN 1000 AND 4999  THEN '3. 1000-4999'
+            WHEN amount < 500 THEN '1. Under 500'
+            WHEN amount BETWEEN 500  AND 999 THEN '2. 500-999'
+            WHEN amount BETWEEN 1000 AND 4999 THEN '3. 1000-4999'
             WHEN amount BETWEEN 5000 AND 19999 THEN '4. 5000-19999'
-            ELSE                                    '5. 20000+'
+            ELSE '5. 20000+'
         END AS amount_band
     FROM transactions
 )
@@ -15,8 +15,8 @@ SELECT
     amount_band,
     COUNT(*) AS total_transactions,
     SUM(is_fraud) AS fraud_count,
-    ROUND(SUM(is_fraud) * 100.0 / COUNT(*), 2) AS fraud_rate_percentage,
-    ROUND(100.0 - (SUM(is_fraud) * 100.0 / COUNT(*)) , 2) AS legit_rate_percentage
+    ROUND(SUM(is_fraud) * 100 / COUNT(*), 2) AS fraud_rate_percentage,
+    ROUND(100 - (SUM(is_fraud) * 100 / COUNT(*)) , 2) AS legit_rate_percentage
 FROM amount_bands
 GROUP BY amount_band
 ORDER BY amount_band;
