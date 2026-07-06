f10c899cc2f17e60261e83674c1a10ebecef43d8
dez-data <samuelasteragondeze@gmail.com>
2026-07-06 03:44:50 +0300

    Update fraud_by_agent_type.sql

--- queries/fraud_by_agent_type.sql
+++ queries/fraud_by_agent_type.sql
@@ -2,8 +2,8 @@ SELECT
     a.agent_type,
     COUNT(t.transaction_id) AS total_transactions,
     SUM(t.is_fraud) AS total_fraud_cases,
-    ROUND(SUM(t.is_fraud) * 100.0 / COUNT(*), 2) AS fraud_rate_percentage,
-    ROUND(SUM(t.amount * t.is_fraud) / 1000.0, 1) AS fraud_value_thousands
+    ROUND(SUM(t.is_fraud) * 100 / COUNT(*), 2) AS fraud_rate_percentage,
+    ROUND(SUM(t.amount * t.is_fraud) / 1000, 1) AS fraud_value_thousands
 FROM transactions t
 JOIN agents a ON t.agent_id = a.agent_id
 GROUP BY a.agent_type
